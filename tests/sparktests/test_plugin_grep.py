51d82284ce50073f3136c54f4f6924344acce85b
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2016-07-12 14:42:38 -0600

    Coalesce Spark RDDs in grep plugin test

--- tests/sparktests/test_plugin_grep.py
+++ tests/sparktests/test_plugin_grep.py
@@ -13,7 +13,7 @@ def test_spark_plugin_grep(searcher, indexer):
     tmp_dir = tempfile.mkdtemp()
 
     try:
-        os.system("spark-submit jobs/spark/index.py --source corpus:%s  --plugin 'plugins.grep.Words:words=c1 d1 world,dir=%s/rdd/'" % (
+        os.system("spark-submit jobs/spark/index.py --source corpus:%s  --plugin 'plugins.grep.Words:words=c1 d1 world,dir=%s/rdd/,coalesce=1'" % (
             pipes.quote(json.dumps(CORPUSES["simple_link_graph_domain"])),
             tmp_dir
         ))
